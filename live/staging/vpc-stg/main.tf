076283909feb62c206bea27d7308dc1208bcb103
triadi80 <triadi80@gmail.com>
2024-09-02 00:41:15 +0700

    add aws vpc stg2

---
+++ live/staging/vpc-stg/main.tf
@@ -0,0 +1,20 @@
+module "vpc" {
+  #source = "terraform-aws-modules/vpc/aws"
+  source = "../../../provisioning/tf-aws-modules/vpc/"
+
+  name = "stg-vpc-b13"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-southeast-3a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.11.0/24"]
+
+  enable_nat_gateway = false
+  single_nat_gateway  = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "stg"
+  }
+}
