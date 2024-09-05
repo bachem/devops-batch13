54f796dce35f726ab25cdaf9c75ee0512f68de58
triadi80 <triadi80@gmail.com>
2024-09-05 11:56:59 +0700

    add ec2

--- live/staging/ec2-db/main.tf
+++ live/staging/ec2-db/main.tf
@@ -1,14 +1,15 @@
 module "ec2_instance" {
   #source  = "terraform-aws-modules/ec2-instance/aws"
-  source  = "../../../provisioning/tf-aws-modules/ec2"
+  source = "../../../provisioning/tf-aws-modules/ec2"
 
   name = "single-instance"
 
-  instance_type          = "t2.micro"
-  key_name               = "user1"
+  ami                    = "ami-06a2e6561950f5040"
+  instance_type          = "t3.micro"
+  key_name               = "devops"
   monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
+  vpc_security_group_ids = ["sg-0dd8a571f32803655"]
+  subnet_id              = "subnet-0ae1ee58bc719d4aa"
 
   tags = {
     Terraform   = "true"
