17e34a573b42543695334cb64c412be01d9728ed
triadi80 <triadi80@gmail.com>
2024-09-16 08:41:23 +0700

    update

--- live/staging/vpc-stg/main.tf
+++ live/staging/vpc-stg/main.tf
@@ -5,9 +5,9 @@ module "vpc" {
   name = "stg-vpc-b13"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-southeast-3a"]
-  private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.11.0/24"]
+  azs             = ["ap-southeast-3a","ap-southeast-3b"]
+  private_subnets = ["10.0.1.0/24","10.0.2.0/24"]
+  public_subnets  = ["10.0.11.0/24","10.0.22.0/24"]
 
   enable_nat_gateway = false
   single_nat_gateway  = false
@@ -16,6 +16,6 @@ module "vpc" {
 
   tags = {
     Terraform = "true"
-    Environment = "stg"
+    Environment = "dev"
   }
 }
