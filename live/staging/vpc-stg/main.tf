6d39405c1275d968f4706601594f4ba2dbaeda23
triadi80 <triadi80@gmail.com>
2024-09-19 19:08:54 +0700

    update

--- live/staging/vpc-stg/main.tf
+++ live/staging/vpc-stg/main.tf
@@ -13,8 +13,10 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24","10.0.2.0/24"]
   public_subnets  = ["10.0.11.0/24","10.0.22.0/24"]
 
-  enable_nat_gateway = false
-  single_nat_gateway  = false
+  #map_public_ip_on_launch = true
+
+  enable_nat_gateway = true
+  single_nat_gateway  = true
   one_nat_gateway_per_az = false
   enable_vpn_gateway = false
 
